245f1449b05f80401c0239f632b09bbb47506dc9
Mariama Sow <sowmariama2385@gmail.com>
2025-07-23 16:09:38 +0000

    modification du fichier settings

--- onlinestore/settings.py
+++ onlinestore/settings.py
@@ -198,4 +198,5 @@ else:
     DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
     MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'  # Gardez cette ligne dans tous les cas
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
